372c65df681e188a1c11e31e2c8dbd0a71e7807b
Aadesh Gupta <aadeshgupta00@gmail.com>
2026-01-15 12:35:29 +0530

    fix payload style

--- src/data.py
+++ src/data.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-def load_filtered_date():
+def load_filtered_data():
 
     df = pd.read_excel("data/master_questions.xlsx", sheet_name="Sheet1")
 
-    print(f'Column names of OG data : {df.columns}')
+    print(f'Column names of OG data : {list(df.columns)}')
     
     required_cols = ['id', 'text', 'answer', 'difficulty', 'text.1']
 
@@ -17,7 +17,32 @@ def load_filtered_date():
                                 "text.1" : "options_text"
                             })
 
-    print(f'Column names of filtered data : {filter_df.columns}')
+    print(f'Column names of filtered data : {list(filter_df.columns)}')
 
     return filter_df
 
+def group_questions(df: pd.DataFrame) -> pd.DataFrame:
+    grouped_df = (
+        df
+        .groupby("question_id", as_index=False)
+        .agg({
+            "question_text": "first",
+            "difficulty": "first"
+        })
+    )
+
+    return grouped_df
+
+def build_llm_payload(grouped_df: pd.DataFrame) -> list[dict]:
+    payload = []
+
+    for i, row in grouped_df.iterrows():
+        payload.append({
+            "reference_question_id": row["question_id"],
+            "question": {
+                "text": row["question_text"],
+                "difficulty": int(row["difficulty"])
+            }
+        })
+
+    return payload
